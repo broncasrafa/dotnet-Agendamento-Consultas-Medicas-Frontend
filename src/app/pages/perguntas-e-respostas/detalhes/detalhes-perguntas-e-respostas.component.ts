80418a932bdd3076cab41b61d00710ce6ce4ac0e
Rafael Francisco <broncasrafa@gmail.com>
2025-02-24 12:38:27 -0300

    ➖ inclusão da tela de detalhes da pergunta

--- src/app/pages/perguntas-e-respostas/detalhes/detalhes-perguntas-e-respostas.component.ts
+++ src/app/pages/perguntas-e-respostas/detalhes/detalhes-perguntas-e-respostas.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
+import { PerguntaResponse } from 'src/app/core/models/perguntas-respostas/response/PerguntaResponse';
+import { NotificationService } from 'src/app/shared/services/notification.service';
+import { PerguntasRespostasService } from 'src/app/core/services/perguntas-respostas.service';
 
 @Component({
   selector: 'app-detalhes-perguntas-e-respostas',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './detalhes-perguntas-e-respostas.component.html',
   styleUrl: './detalhes-perguntas-e-respostas.component.css'
 })
-export class PerguntasERespostasDetalhesComponent {
+export class PerguntasERespostasDetalhesComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
 
+  private route = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private notificationService = inject(NotificationService);
+  private perguntaRespostaService = inject(PerguntasRespostasService);
+
+  perguntaId: number;
+  pergunta: PerguntaResponse;
+
+  constructor() {
+    this.perguntaId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    this.getPerguntaDetalhes();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  getPerguntaDetalhes() {
+    this.perguntaRespostaService.getPerguntaById(this.perguntaId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.pergunta = response.data!;
+          console.log(this.pergunta);
+        },
+        error: (error) => this.notificationService.showHttpResponseErrorNotification(error)
+      });
+  }
 }
