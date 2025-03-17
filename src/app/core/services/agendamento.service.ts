d5ea1f9e6bbf884ec1eb4586d3ef6c991efede28
Rafael Francisco <broncasrafa@gmail.com>
2025-03-17 08:16:36 -0300

    📌 finalizado a tela de detalhes do agendamentos de consultas do paciente

--- src/app/core/services/agendamento.service.ts
+++ src/app/core/services/agendamento.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ApiResponse } from 'src/app/core/models/ApiResponse';
 import { CreateAgendamentoRequest } from 'src/app/core/models/agendamento/request/CreateAgendamentoRequest';
+import { AgendamentoResponse } from 'src/app/core/models/agendamento/response/AgendamentoResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,15 @@ export class AgendamentoService {
    */
   createAgendamento(request: CreateAgendamentoRequest): Observable<ApiResponse<number>> {
     return this.http.post<ApiResponse<number>>(`${this.base_api_url}/`, request);
+  }
+
+  /**
+   * Obter os dados do agendamento pelo ID especificado
+   * @param id identificador do agendamento
+   * @returns os dados do agendamento
+   */
+  getAgendamentoById(id: Number): Observable<AgendamentoResponse> {
+    return this.http.get<ApiResponse<AgendamentoResponse>>(`${this.base_api_url}/${id}`, { responseType: 'json'})
+            .pipe(map(response => response.data!));
   }
 }
--- src/app/pages/paciente/detalhes-consultas/consultas-paciente-detalhes.component.ts
+++ src/app/pages/paciente/detalhes-consultas/consultas-paciente-detalhes.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
+
+import { AppUtils } from 'src/app/core/utils/app.util';
+import { DateFormattedPipe } from 'src/app/shared/pipes/date-formatted.pipe';
+import { AddressFormattedPipe } from 'src/app/shared/pipes/address-formatted.pipe';
+import { LoadingService } from 'src/app/shared/services/loading.service';
+import { CryptoService } from 'src/app/shared/services/crypto.service';
+import { NotificationService } from 'src/app/shared/services/notification.service';
+import { AgendamentoService } from 'src/app/core/services/agendamento.service';
+import { AgendamentoResponse } from 'src/app/core/models/agendamento/response/AgendamentoResponse';
 
 @Component({
   selector: 'app-consultas-paciente-detalhes',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    DateFormattedPipe,
+    AddressFormattedPipe,
+  ],
   templateUrl: './consultas-paciente-detalhes.component.html',
   styleUrl: './consultas-paciente-detalhes.component.css'
 })
-export class ConsultasPacienteDetalhesComponent {
+export class ConsultasPacienteDetalhesComponent implements OnInit, OnDestroy {
 
+  private destroy$ = new Subject<void>();
+
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private notificationService = inject(NotificationService);
+  private agendamentoService = inject(AgendamentoService);
+  private cryptoService = inject(CryptoService);
+  private loadingService = inject(LoadingService);
+
+  agendamentoId: number;
+  agendamento: AgendamentoResponse;
+
+  constructor() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params['agendamentoId']) {
+        this.agendamentoId = this.cryptoService.descriptografar(params['agendamentoId']);
+        this.obterDadosAgendamento();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();// Limpeza de memória
+  }
+
+  loadDataIsReady() {
+    return !this.isNullOrUndefined(this.agendamento);
+  }
+
+  isNullOrEmpty(value: any): boolean {
+    return AppUtils.isNullOrEmpty(value);
+  }
+  isNullOrUndefined(value: any): boolean {
+    return AppUtils.isNullOrUndefined(value);
+  }
+
+  obterDadosAgendamento() {
+    this.agendamentoService.getAgendamentoById(this.agendamentoId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.agendamento = response
+          console.log(this.agendamento)
+        },
+        error: err => this.notificationService.showHttpResponseErrorNotification(err)
+    });
+  }
+
+  onclick_VoltarHistorico() {
+    this.router.navigate(['/paciente/consultas']);
+  }
 }
